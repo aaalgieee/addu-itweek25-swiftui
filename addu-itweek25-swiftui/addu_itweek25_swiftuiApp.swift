05b09d19477fabfc3fc97db3ddf81ec47066030f
Al Gabriel Orig <jaworski.orig@gmail.com>
2025-02-22 18:49:00 +0800

    hello world

---
+++ addu-itweek25-swiftui/addu_itweek25_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  addu_itweek25_swiftuiApp.swift
+//  addu-itweek25-swiftui
+//
+//  Created by Al Gabriel on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct addu_itweek25_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
